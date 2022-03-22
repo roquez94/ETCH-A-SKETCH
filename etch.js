82bddd7592c4391d6121a2ca11624f84dd5956e2
Roquez Walker <97702021+roquez94@users.noreply.github.com>
2022-03-21 22:21:16 -0400

    Added resetGrid button; makes new/reset grid

--- etch.js
+++ etch.js
@@ -2,12 +2,14 @@ const grid = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cells");
 
+let rowNum;
+let cellNum;
 
 gridMaker();
 //Creates a default grid sized 16x16 
 function gridMaker() {
-    makeRows(16);
-    makeColumns(16);
+    makeRows(rowNum);
+    makeColumns(cellNum);
 }
 
 //Takes (rows, columns) input and makes a grid
@@ -17,20 +19,36 @@ function makeRows(rowNum) {
     for (r = 0; r < rowNum; r++) {
         let row = document.createElement("div");
         grid.appendChild(row).className = "gridRow";
+       
     }
 }
 
 //Creates columns
+
 function makeColumns(cellNum) {
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cells";
+            
+            //a “hover” effect so that the grid divs change color
+            
+            newCell.addEventListener('mouseover', e => e.target.classList.add('hover-color'))
+            newCell.addEventListener('mouseout', e => e.target.classList.remove('hover-color'))
         }
 
     }
 }
+//resets grid and prompts for new dimension size
+let reset = document.getElementById("reset");
+reset.addEventListener('click', resetGrid);
 
-//a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail
-
-
+function resetGrid () {
+	let resetRow = prompt("Enter new row size under 100") ;
+  let resetColumn = prompt("Enter new column size under 100");
+  
+  rowNum = resetRow;
+  cellNum = resetColumn;
+  
+  gridMaker();
+}
