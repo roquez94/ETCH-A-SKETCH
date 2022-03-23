e9a0a81f08003d46747a59a45cff65d029b8ca27
Roquez Walker <97702021+roquez94@users.noreply.github.com>
2022-03-22 20:10:34 -0700

    added reset and new grid functions

--- etch.js
+++ etch.js
@@ -5,7 +5,6 @@ let cells = document.getElementsByClassName("cells");
 let rowNum;
 let cellNum;
 
-gridMaker();
 //Creates a default grid sized 16x16 
 function gridMaker() {
     makeRows(rowNum);
@@ -30,25 +29,34 @@ function makeColumns(cellNum) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cells";
-            
-            //a “hover” effect so that the grid divs change color
-            
+
+            //a “hover” effect so that the grid divs change color  
             newCell.addEventListener('mouseover', e => e.target.classList.add('hover-color'))
             newCell.addEventListener('mouseout', e => e.target.classList.remove('hover-color'))
         }
-
     }
 }
-//resets grid and prompts for new dimension size
+//resets - clears grid
 let reset = document.getElementById("reset");
 reset.addEventListener('click', resetGrid);
 
 function resetGrid () {
-	let resetRow = prompt("Enter new row size under 100") ;
-  let resetColumn = prompt("Enter new column size under 100");
-  
-  rowNum = resetRow;
-  cellNum = resetColumn;
-  
+  grid.querySelectorAll('*').forEach(n => n.remove());
+}
+
+//creates new grid and prompts for new dimension size (also clears)
+let newPlot = document.getElementById("new");
+newPlot.addEventListener('click', newGrid);
+
+function newGrid () {
+  grid.querySelectorAll('*').forEach(n => n.remove());
+
+	rowNum = prompt("Enter new grid size under 100") ;
+  if (rowNum >=101) {
+    alert ("Must enter grid size under 100");
+    return;
+  } else { 
+  cellNum = rowNum;
+  }
   gridMaker();
 }
